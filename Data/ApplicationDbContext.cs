663cffaab80d3f0d61cd2af7e4c9ea5603c026ff
Patrick Gannon <gannon.patrick01@gmail.com>
2022-04-07 23:45:16 -0400

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPizzeria.Models;
+
+namespace RazorPizzeria.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+		public DbSet<PizzaOrder> PizzaOrders { get; set; }
+
+		// base() calls the constructor of the class we inherit from, which we very much need to do in this case
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+		{
+
+		}
+	}
+}
